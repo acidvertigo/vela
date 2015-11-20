3f24947b82cf60a6e8b735a8efd8ff36ad43bbe2
Luca <ldanzelmo@gmail.com>
2015-11-20 15:41:47 +0100

    Create User.php

---
+++ src/Core/User.php
@@ -0,0 +1,41 @@
+<?php
+namespace Vela\Core;
+
+use \Sabre\HTTP\Request;
+
+Class User
+{
+  private $request;
+  
+  public function __construct(Request $request)
+  {
+      $this->request = $request;
+  }
+  
+  public function getUserAgent()
+  {
+      return $this->request->getRawServerValue('HTTP_USER_AGENT');
+  }
+
+  public function getUserIp()
+  {
+    $userIp = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $userIp = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $userIp = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $userIp = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $userIp = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+        $userIp = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $userIp = getenv('REMOTE_ADDR');
+    else
+        $userIp = 'UNKNOWN';
+
+    return $userIp;
+  }
+
+}
