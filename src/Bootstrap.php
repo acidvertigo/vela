cacef40fb4771a35745a0bc32759106314fda1d1
Acidvertigo <ldanzelmo@gmail.com>
2015-11-20 23:18:09 +0100

    add closure

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -15,7 +15,9 @@ $environment = 'development';
 $whoops = new \Whoops\Run;
 if ($environment !== 'production')
 {
-    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+    $whoops->pushHandler(function () {
+        return new \Whoops\Handler\PrettyPageHandler;
+    });
 } else
 {
     $whoops->pushHandler(function() {
