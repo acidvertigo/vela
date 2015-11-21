7babd71883e732fe0d9d9c7166b9a832c37e6162
acidvertigo <ldanzelmo@gmail.com>
2015-11-21 19:38:23 +0100

    Add default database config

--- src/Config/Production/Config.php
+++ src/Config/Production/Config.php
@@ -1,6 +1,12 @@
 <?php
 
 return [
+	'database' => [
+	    'host' => 'localhost',
+		'db_name' => 'test',
+		'user' => 'root',
+		'password' => ''
+	],          
     'locale' => [
 	    'timezone' => 'Europe/Rome',
 		'default_language' => 'en'
