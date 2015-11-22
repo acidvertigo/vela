28f29105f5a5f683d440bf3b9025051ec62608e4
Acidvertigo <ldanzelmo@gmail.com>
2015-11-22 17:02:42 +0100

    replace tabs with spaces

--- src/Config/Production/Config.php
+++ src/Config/Production/Config.php
@@ -1,17 +1,17 @@
 <?php
 
 return [
-	'database' => [
-	    'host' => 'localhost',
-		'db_name' => 'test',
-		'user' => 'root',
-		'password' => ''
-	],          
+    'database' => [
+        'host' => 'localhost',
+        'db_name' => 'test',
+        'user' => 'root',
+        'password' => ''
+    ],          
     'locale' => [
-	    'timezone' => 'Europe/Rome',
-		'default_language' => 'en'
-	],
-	'session' => [
-		'id' => 'vela_id'
-	]	            
+        'timezone' => 'Europe/Rome',
+        'default_language' => 'en'
+    ],
+    'session' => [
+        'id' => 'vela_id'
+    ]            
 ];
