c240c2578b513f73ea82d882e267c9c537fba66b
acidvertigo <ldanzelmo@gmail.com>
2015-11-23 01:11:30 +0100

    Fix last commit

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -7,7 +7,7 @@ require __DIR__.'/../vendor/autoload.php';
 error_reporting(E_ALL|E_STRICT);
 
 // define environment
-$environment = 'Production'; // Accepted values: Development or Production.
+$environment = 'Development'; // Accepted values: Development or Production.
 
 // Load configuration file
 $config = require 'Config/' . $environment . '/Config.php';
--- src/Config/Production/Routes.php
+++ src/Config/Production/Routes.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    ['GET', '/vela-commerce', ['Vela\Controllers\Homepage', 'show']],
+    ['GET', '/', ['Vela\Controllers\Homepage', 'show']],
 ];
